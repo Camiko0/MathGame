47adb034f793086d292a7979fa79da00f754bbb1
Camiko0 <kristianiz562@gmail.com>
2020-06-21 17:47:28 -0500

    Creación de la tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'MathGame';
+  operaciones = [[1,2,3],[4,5,6],[7,8,9]];
+  resultados = [null,null,null];
+  isVisible = false;
+  error: string = null;
+
+  closeError() {
+    this.error = null;
+  }
+
+  hacerOperacionVerdadero(operacion, numeros){
+    if (operacion == "SUMA"){
+      if (this.operaciones[numeros][0] + this.operaciones[numeros][1] == this.operaciones[numeros][2]){
+        this.resultados[numeros] = true;
+      }
+      else {
+        this.resultados[numeros] = false;
+      }
+    }
+  }
+
+  hacerOperacionFalso(operacion, numeros){
+    if (operacion == "SUMA"){
+      if (this.operaciones[numeros][0] + this.operaciones[numeros][1] != this.operaciones[numeros][2]){
+        this.resultados[numeros] = true;
+      }
+      else {
+        this.resultados[numeros] = false;
+      }
+    }
+  }
 }
